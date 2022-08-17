41e5159cfa84ef02a2fbf7d731b7b0878601a0e3
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-08-16 22:20:33 -0300

    Add repository

---
+++ src/Repository/BookRepository.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using Repository.DatabaseAccess;
+
+namespace Repository.Data;
+
+public class BookRepository : IBookRepository
+{
+    private readonly IDatabaseAccess _db;
+
+    public BookRepository(IDatabaseAccess db) => _db = db;
+
+    public async Task<IEnumerable<Book>> GetBooksAsync()
+        => await _db.GetDataAsync<Book, object>("dbo.GetAllBooksSP", new { });
+
+    public async Task<Book?> GetBookAsync(int id) =>
+        (await _db.GetDataAsync<Book, object>("dbo.GetBookByIdSP", new { Id = id })).FirstOrDefault();
+
+    public async Task InsertBookAsync(Book book) =>
+        await _db.SaveDataAsync("dbo.InsertBookSP", new { book.Name, book.Description, book.ISBN, book.PublishedAt });
+
+    public async Task UpdateBookAsync(Book book) =>
+        await _db.SaveDataAsync("dbo.UpdateBookSP", book);
+
+    public async Task DeleteBookAsync(int id) =>
+        await _db.SaveDataAsync("dbo.DeleteBookSP", new { Id = id });
+}
---
+++ src/Repository/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+
+namespace Repository;
+
+public interface IBookRepository
+{
+    Task DeleteBookAsync(int id);
+    Task<Book?> GetBookAsync(int id);
+    Task<IEnumerable<Book>> GetBooksAsync();
+    Task InsertBookAsync(Book book);
+    Task UpdateBookAsync(Book book);
+}
