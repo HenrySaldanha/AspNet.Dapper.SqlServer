9cde9d87fa3e2b7480808bd9380652607486c0a8
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-08-16 22:20:26 -0300

    Add implementation for database access

---
+++ src/Repository/DatabaseAccess/DatabaseAccess.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Repository.DatabaseAccess;
+
+public class DatabaseAccess : IDatabaseAccess
+{
+    private readonly string _connectionString;
+    public DatabaseAccess(IConfiguration config) => _connectionString = config.GetConnectionString("SqlServer");
+
+    public async Task<IEnumerable<T>> GetDataAsync<T, U>(string storedProcedure, U parameters)
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+
+        return await connection.QueryAsync<T>
+            (storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task SaveDataAsync<T>(string storedProcedure, T parameters)
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+
+        await connection.ExecuteAsync
+            (storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+}
---
+++ src/Repository/DatabaseAccess/IDatabaseAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace Repository.DatabaseAccess;
+
+public interface IDatabaseAccess
+{
+    Task<IEnumerable<T>> GetDataAsync<T, U>(string storedProcedure, U parameters);
+    Task SaveDataAsync<T>(string storedProcedure, T parameters);
+}
