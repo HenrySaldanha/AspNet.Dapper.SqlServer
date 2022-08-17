58c8960f744bc96b2994ff40f811c6030caeef60
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-08-16 22:20:11 -0300

    Add Domain project

---
+++ src/Domain/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ISBN { get; set; }
+    public DateTime PublishedAt { get; set; }
+}
